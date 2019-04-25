43a88ccff972859e01cfa915e24d941465c0dd54
israxx97 <isragarcia97@gmail.com>
2019-04-26 00:26:30 +0200

    + v0.3.1 - 26/04/2019, 00:25

---
+++ view/vWIP.php
@@ -0,0 +1,7 @@
+<link rel="stylesheet" type="text/css" href="webroot/css/vWIPStyles.css">
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <form name="formWIP" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+        <input type="submit" class="btn btn-secondary my-2 my-sm-0 btn-sm" name="cancelar" value="Volver Inicio">
+    </form>
+</nav>
+<p class="h2 text-center">Work in Progress!</p>
